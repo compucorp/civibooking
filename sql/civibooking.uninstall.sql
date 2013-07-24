d6e492f7bdb5881dab5f306fcf462555dc77c10d
Erawat Chamanont <erawat@gmail.com>
2013-07-24 17:08:19 +0100

    change order for dropping tables

--- sql/civibooking.uninstall.sql
+++ sql/civibooking.uninstall.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS `civicrm_booking`;
 DROP TABLE IF EXISTS `civicrm_booking_config`;
 DROP TABLE IF EXISTS `civicrm_booking_slot`;
 DROP TABLE IF EXISTS `civicrm_booking_cancellation`;
 DROP TABLE IF EXISTS `civicrm_booking_payment`;
-DROP TABLE IF EXISTS `civicrm_booking_resource`;
 DROP TABLE IF EXISTS `civicrm_booking_resource_criteria`;
 DROP TABLE IF EXISTS `civicrm_booking_resource_config`;
+DROP TABLE IF EXISTS `civicrm_booking_resource`;
+DROP TABLE IF EXISTS `civicrm_booking`;
