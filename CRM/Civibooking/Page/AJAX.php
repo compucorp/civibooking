9319d7f20df04667abaa3cef10e38392690c9bf3
Erawat Chamanont <erawat@gmail.com>
2013-07-31 15:02:33 +0100

    ajax Class CiviBooking

---
+++ CRM/Civibooking/Page/AJAX.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.3                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+
+/**
+ * This class contains all the function that are called using AJAX
+ */
+class CRM_Civibooking_Page_AJAX {
+
+  /**
+   * Function to search resouce when adding resource to booking
+   * 
+   */
+  static function searchResourch() {
+    $recordID  = CRM_Utils_Type::escape($_POST['resourceID'], 'Integer');
+    $recordBAO = CRM_Utils_Type::escape($_POST['resourceType'], 'String');
+    $op        = CRM_Utils_Type::escape($_POST['op'], 'String');
+    $show      = NULL;
+
+    echo json_encode('ajax');
+    CRM_Utils_System::civiExit();
+  }
+
+ 
+}
+
