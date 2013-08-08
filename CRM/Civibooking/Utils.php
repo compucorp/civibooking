0abbc2b6da1356cefe6b2c3c6ab4b634a55707a4
Erawat Chamanont <erawat@gmail.com>
2013-08-08 10:12:09 +0100

    Implement create time rang on units class

--- CRM/Civibooking/Utils.php
+++ CRM/Civibooking/Utils.php
@@ -36,5 +36,35 @@
 
 class CRM_Civibooking_Utils {
 
+
+    /** 
+   * create_time_range  
+   *  
+   * @param mixed $start start time, e.g., 9:30am or 9:30 
+   * @param mixed $end   end time, e.g., 5:30pm or 17:30 
+   * @param string $by   1 hour, 1 mins, 1 secs, etc. 
+   * @access public 
+   * @return void 
+   */ 
+  static function createTimeRange($start, $end, $by='30 mins') { 
+
+      $start_time = strtotime($start); 
+      $end_time   = strtotime($end); 
+
+      $current    = time(); 
+      $add_time   = strtotime('+'.$by, $current); 
+      $diff       = $add_time-$current; 
+
+      $times = array(); 
+      while ($start_time < $end_time) { 
+          $times[] = $start_time; 
+          $start_time += $diff; 
+      } 
+      $times[] = $start_time; 
+      return $times; 
+  } 
+
+
+
  
 }
