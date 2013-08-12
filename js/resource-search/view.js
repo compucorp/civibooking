df18792270af1ce92724ee8cfbb1a317fe0740d9
Erawat Chamanont <erawat@gmail.com>
2013-08-12 09:58:58 +0100

    AddToBasket view

--- js/resource-search/view.js
+++ js/resource-search/view.js
@@ -21,14 +21,38 @@ CRM.ResourceSearch.module('View', function(View, ResourceSearch, Backbone, Mario
 
   });
 
-  View.AddToBasket = Backbone.Marionette.ItemView.extend({
+  View.Basket = Backbone.Marionette.ItemView.extend({
+    template: "#basket-table-template",
+ 
+    initialize: function(){},
+    events: {
+     
+    },
+   
+  })
+
+  View.AddToBasketModal = Backbone.Marionette.ItemView.extend({
     template: "#add-to-basket-form-template",
     className: "modal-dialog",
     events: {
       "click #addToBasket": "addToBasket",
     },
-    addToBasket: function(){
+    addToBasket: function(e){
+      ResourceSearch.modal.reset();
+      var view = new View.Basket();
+      ResourceSearch.basket.show(view);
+      //var select = $("select[name='resources']");
+      //append object to multiselect
+      //console.log($('#hiddenValue'));
+      //$('#hiddenValue').append('<input name="resources[]" type="text" id="resources" class="form-text value="yeah">');
+      /*
 
+      $('#resources').append($('<option>', { 
+        value: 'value',
+        text : 'value',
+        selected: true, 
+      }));*/
+      $('#resources').append('yeah\n');
     }
 
   });
@@ -42,8 +66,11 @@ CRM.ResourceSearch.module('View', function(View, ResourceSearch, Backbone, Mario
     events: {
       'click .add-to-basket': 'loadForm'
     },
-    loadForm: function(){
-     var view = new View.AddToBasket();
+    loadForm: function(e){
+     var ref = $(e.currentTarget).data('ref');
+     var date = $(e.currentTarget).data('date');
+     var model = new ResourceSearch.Model.Basket({id: ref, date: date});
+     var view = new View.AddToBasketModal();
      ResourceSearch.modal.show(view);
   
     }
@@ -53,24 +80,27 @@ CRM.ResourceSearch.module('View', function(View, ResourceSearch, Backbone, Mario
 
   View.ResourceTable = Backbone.Marionette.CompositeView.extend({
     template: '#search-result-table-template',
-    itemViewContainer: "tbody",
+    //itemViewContainer: "tbody",
     itemView: View.ResourceRow,
   
     initialize: function(){
+      //set view id
       this.id = this.model.get('result').date_timestamp;
+      var date = this.model.get('result').date;
       var resources = [];
       _.each(this.model.get('result').resources, function (resource){
-        resources[resources.length] = new ResourceSearch.Model.Resource({
+        resources[resources.length] = new ResourceSearch.Model.ResourceRow({
           id: resource.id,
           description: resource.description,
           is_unlimited: resource.is_unlimited,
           label: resource.label,
           resource_location: resource.resource_location,
           resource_type: resource.resource_type,
+          date: date, //added date to a row so we know that date is seleted.
           slots: resource.slots,
         });
       });
-      this.collection = new ResourceSearch.Collection.ResourceList(resources);
+      this.collection = new ResourceSearch.Collection.ResourceTableList(resources);
     },
     
     
