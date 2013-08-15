ca8a7b64fb22c6617cc99ac5864fd00636e7cb2b
Erawat Chamanont <erawat@gmail.com>
2013-08-15 15:22:43 +0100

    Remove Booking from the APIs class name

---
+++ api/v3/Resource.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * BookingResource.Get API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_resource_create_spec(&$spec) {
+  //$spec['magicword']['api.required'] = 1;
+}
+
+/**
+ * Resource.Create API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_resource_create($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
---
+++ api/v3/ResourceConfigOption.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * BookingResourceConfigOption.Create API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_resource_config_option_create_spec(&$spec) {
+  //$spec['magicword']['api.required'] = 1;
+}
+
+/**
+ * BookingResourceConfigOption.Create API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_resource_config_option_create($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
