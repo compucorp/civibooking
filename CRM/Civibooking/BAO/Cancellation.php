579692ab926e7a225f4672ef159c3f4739d60918
Erawat Chamanont <erawat@gmail.com>
2013-08-15 11:48:04 +0100

    added BAOs

---
+++ CRM/Civibooking/BAO/Cancellation.php
@@ -0,0 +1,27 @@
+<?php
+
+class CRM_Civibooking_BAO_Cancellation extends CRM_Civibooking_DAO_Cancellation {
+
+
+    /**
+   * takes an associative array and creates a resource object
+   *
+   * the function extract all the params it needs to initialize the create a
+   * resource object. the params array could contain additional unused name/value
+   * pairs
+   *
+   * @param array $params (reference ) an assoc array of name/value pairs
+   * @param array $ids    the array that holds all the db ids
+   *
+   * @return object CRM_Civibooking_BAO_Resource object
+   * @access public
+   * @static
+   */
+  static function create(&$params) {
+    $resourceDAO = new CRM_Civibooking_DAO_Cancellation();
+    $resourceDAO->copyValues($params);
+    return $resourceDAO->save();
+  }
+
+
+}
