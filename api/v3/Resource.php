a7b9ea1b9f36bbb061e0bfc52081bf0bb85d1351
Erawat Chamanont <erawat@gmail.com>
2013-08-16 18:31:06 +0100

    Implement Resource.Get APIs

--- api/v3/Resource.php
+++ api/v3/Resource.php
@@ -25,3 +25,20 @@ function civicrm_api3_resource_create($params) {
   return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
 
+
+/**
+ * Resource.Get API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_resource_get($params) {
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+
+
+
