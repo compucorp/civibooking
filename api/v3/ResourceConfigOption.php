5601a9d617003bb654f9d43ee4099b51f0467778
Erawat Chamanont <erawat@gmail.com>
2013-08-16 15:52:36 +0100

    added Get API for resource config option

--- api/v3/ResourceConfigOption.php
+++ api/v3/ResourceConfigOption.php
@@ -25,3 +25,19 @@ function civicrm_api3_resource_config_option_create($params) {
   return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
 
+/**
+ * BookingResourceConfigOptiont.Get API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_resource_config_option_get($params) {
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+
+
+
