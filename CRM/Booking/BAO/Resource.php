71b1ba1b4fa6c454c13a2d45df367d97a147eab2
Erawat Chamanont <erawat@gmail.com>
2013-08-18 21:47:01 +0100

    updated option group name

--- CRM/Booking/BAO/Resource.php
+++ CRM/Booking/BAO/Resource.php
@@ -3,16 +3,14 @@
 class CRM_Booking_BAO_Resource extends CRM_Booking_DAO_Resource {
 
   static function getResourceTypeGroupId(){
-    $result = civicrm_api('OptionGroup', 'get',array('version' => 3, 'name' => 'Booking_resource_type'));
+    $result = civicrm_api('OptionGroup', 'get',array('version' => 3, 'name' => 'booking_resource_type'));
     $typeGroupId = $result['id'];
-
     return $typeGroupId;
   }
 
   static function getResourcesByType($type) {
 
     $typeGroupId = self::getResourceTypeGroupId();
-
     $params = array(1 => array( $type, 'String'));
     $query = "
     SELECT civicrm_booking_resource.id,
@@ -45,34 +43,66 @@ class CRM_Booking_BAO_Resource extends CRM_Booking_DAO_Resource {
   }
 
 
+
+
   static function getResourceTypes($includeLimited = true){
 
     $typeGroupId = self::getResourceTypeGroupId();
+    if($typeGroupId){
 
-    $whereClause = " WHERE 1";
-    if (!$includeLimited) {
-        $whereClause .= " AND civicrm_booking_resource.is_unlimited = 0";
+      $whereClause = " WHERE 1";
+      if (!$includeLimited) {
+          $whereClause .= " AND civicrm_booking_resource.is_unlimited = 0";
+      }
+
+      $query = "
+          SELECT civicrm_option_value.label, civicrm_option_value.value, civicrm_option_value.option_group_id
+          FROM civicrm_option_value
+          INNER JOIN civicrm_booking_resource ON civicrm_option_value.option_group_id = $typeGroupId
+          AND civicrm_booking_resource.type_id = civicrm_option_value.id";
+
+       $query .= "$whereClause";
+
+      $resourceTypes = array();
+      $dao = CRM_Core_DAO::executeQuery($query);
+      while ($dao->fetch()) {
+         $resourceTypes[$dao->value] = array(
+          'label' => $dao->label,
+          'value' => $dao->value,
+          'option_group_id' => $dao->option_group_id
+        );
+      }
+      return $resourceTypes;
+    }else{
+      CRM_Core_Error::fatal('Civibooking resource type option group appears to be missing.');
     }
-
-    $query = "
-        SELECT civicrm_option_value.label, civicrm_option_value.value, civicrm_option_value.option_group_id
-        FROM civicrm_option_value
-        INNER JOIN civicrm_booking_resource ON civicrm_option_value.option_group_id = $typeGroupId
-        AND civicrm_booking_resource.resource_type = civicrm_option_value.value";
-
-     $query .= "$whereClause";
-
-    $resourceTypes = array();
-    $dao = CRM_Core_DAO::executeQuery($query);
-    while ($dao->fetch()) {
-       $resourceTypes[$dao->value] = array(
-        'label' => $dao->label,
-        'value' => $dao->value,
-        'option_group_id' => $dao->option_group_id
-      );
-    }
-    return $resourceTypes;
 
   }
+
+
+  /**
+   * Unsets the Resource's is_active flag in the database
+   */
+  public function disable() {
+    $this->is_enabled = 0;
+    $this->save();
+  }
+
+    /**
+   * Sets the Resource's is_active flag in the database
+   */
+  public function enable() {
+    $this->is_enabled = 1;
+    $this->save();
+  }
+
+  /**
+   * Sets the Resource's is_deleted flag in the database
+   */
+  public function delete() {
+    $this->is_deleted = 1;
+    $this->save();
+  }
+
 
 }
