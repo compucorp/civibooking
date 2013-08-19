c7ed7bbde48c31d77f960d97b5d7a969cb0dc1a0
Erawat Chamanont <erawat@gmail.com>
2013-08-19 11:58:27 +0100

    updated SQL from generated DAO

--- sql/civibooking_install.sql
+++ sql/civibooking_install.sql
@@ -22,8 +22,8 @@ CREATE TABLE `civicrm_booking` (
 
 
      `id` int unsigned NOT NULL AUTO_INCREMENT  ,
-     `primary_contact_id` int unsigned    COMMENT 'FK to Contact ID',
-     `secondary_contact_id` int unsigned    COMMENT 'FK to Contact ID',
+     `primary_contact_id` int unsigned NOT NULL   COMMENT 'FK to Contact ID',
+     `secondary_contact_id` int unsigned NOT NULL   COMMENT 'FK to Contact ID',
      `po_number` varchar(255) NOT NULL   ,
      `status_id` int unsigned NOT NULL   ,
      `title` varchar(50) NOT NULL   ,
@@ -33,7 +33,7 @@ CREATE TABLE `civicrm_booking` (
      `participants_estimate` varchar(255)    ,
      `participants_actual` varchar(255)    ,
      `payment_status` int unsigned    ,
-     `is_deleted` tinyint   DEFAULT 0 ,
+     `is_deleted` tinyint    ,
      `created_by` datetime NOT NULL   ,
      `created_date` int unsigned NOT NULL   ,
      `updated_by` int unsigned NOT NULL   ,
@@ -100,16 +100,14 @@ CREATE TABLE `civicrm_booking_resource_config_set` (
      `id` int unsigned NOT NULL AUTO_INCREMENT  ,
      `title` varchar(255) NOT NULL   ,
      `weight` int unsigned NOT NULL   ,
-     `is_enabled` tinyint NOT NULL  DEFAULT 1 ,
-     `is_deleted` tinyint NOT NULL  DEFAULT 0
+     `is_active` tinyint NOT NULL   ,
+     `is_deleted` tinyint
 ,
     PRIMARY KEY ( `id` )
 
 
 
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
-
-
 
 
 -- /*******************************************************
@@ -122,14 +120,14 @@ CREATE TABLE `civicrm_booking_resource` (
 
      `id` int unsigned NOT NULL AUTO_INCREMENT  ,
      `set_id` int unsigned    COMMENT 'FK to resource configuration option set',
-     `label` varchar(255)  ,
+     `label` varchar(255)    ,
      `description` varchar(255)    ,
      `weight` int NOT NULL   ,
-     `resource_type` varchar(512) NOT NULL   COMMENT 'Type of resource=, link to option group',
-     `resource_location` varchar(512) NOT NULL   COMMENT 'Type of resource location, link to option group',
-     `is_unlimited` tinyint   DEFAULT 0 ,
-     `is_enabled` tinyint   DEFAULT 1 ,
-     `is_deleted` tinyint   DEFAULT 0
+     `type_id` int unsigned    COMMENT 'The type associated with this resource. Implicit FK to option_value row in booking_resource_type option_group.',
+     `location_id` int unsigned    COMMENT 'The location associated with this resource. Implicit FK to option_value row in booking_resource_location option_group.',
+     `is_unlimited` tinyint NOT NULL   ,
+     `is_active` tinyint NOT NULL   ,
+     `is_deleted` tinyint
 ,
     PRIMARY KEY ( `id` )
 
@@ -150,9 +148,9 @@ CREATE TABLE `civicrm_booking_resource_config_option` (
      `label` varchar(255) NOT NULL   ,
      `price` decimal(20,2) NOT NULL   ,
      `max_size` varchar(255)    ,
-     `unit_id` varchar(512) NOT NULL   COMMENT 'Type of units, link to option group',
+     `unit_id` int unsigned    COMMENT 'The unit associated with this config option. Implicit FK to option_value row in booking_size_unit option_group.',
      `weight` int unsigned NOT NULL   ,
-     `is_enabled` tinyint NOT NULL  DEFAULT 1
+     `is_active` tinyint NOT NULL
 ,
     PRIMARY KEY ( `id` )
 
@@ -194,14 +192,15 @@ CREATE TABLE `civicrm_booking_slot` (
      `quantity` int NOT NULL   ,
      `note` text    ,
      `parent_id` int unsigned    ,
-     `is_cancelled` tinyint NOT NULL  DEFAULT 0 ,
-     `is_deleted` tinyint NOT NULL  DEFAULT 0
+     `is_cancelled` tinyint    ,
+     `is_deleted` tinyint
 ,
     PRIMARY KEY ( `id` )
 
 
 ,          CONSTRAINT FK_civicrm_booking_slot_booking_id FOREIGN KEY (`booking_id`) REFERENCES `civicrm_booking`(`id`) ON DELETE CASCADE,          CONSTRAINT FK_civicrm_booking_slot_config_id FOREIGN KEY (`config_id`) REFERENCES `civicrm_booking_resource_config_option`(`id`) ON DELETE CASCADE
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+
 
 -- /*******************************************************
 -- *
@@ -220,3 +219,5 @@ CREATE TABLE `civicrm_booking_payment` (
 
 ,          CONSTRAINT FK_civicrm_booking_payment_booking_id FOREIGN KEY (`booking_id`) REFERENCES `civicrm_booking`(`id`) ON DELETE CASCADE,          CONSTRAINT FK_civicrm_booking_payment_contribution_id FOREIGN KEY (`contribution_id`) REFERENCES `civicrm_contribution`(`id`) ON DELETE CASCADE
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+
+
--- sql/civibooking_uninstall.sql
+++ sql/civibooking_uninstall.sql
@@ -10,39 +10,39 @@ DROP TABLE IF EXISTS `civicrm_booking_cancellation`;
 DROP TABLE IF EXISTS `civicrm_booking_config`;
 DROP TABLE IF EXISTS `civicrm_booking`;
 
-DELETE FROM civicrm_option_value WHERE option_group_id = 2 AND name = 'civibooking_acivity_booking';
+DELETE FROM civicrm_option_value WHERE option_group_id = 2 AND name = 'booking_acivity_booking';
 
 DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'civibooking_booking_status' ;
+WHERE civicrm_option_group.name = 'booking_booking_status' ;
 
 DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'civibooking_resource_type';
+WHERE civicrm_option_group.name = 'booking_resource_type';
 
 DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'civibooking_resource_criteria';
+WHERE civicrm_option_group.name = 'booking_resource_criteria';
 
 DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'civibooking_resource_location';
+WHERE civicrm_option_group.name = 'booking_resource_location';
 
 DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'civibooking_cancellation_charges';
+WHERE civicrm_option_group.name = 'booking_cancellation_charges';
 
 DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'civibooking_size_unit';
+WHERE civicrm_option_group.name = 'booking_size_unit';
 
 --all option groups related to civibooking
-DELETE FROM civicrm_option_group WHERE name = 'civibooking_booking_status';
-DELETE FROM civicrm_option_group WHERE name = 'civibooking_resource_type';
-DELETE FROM civicrm_option_group WHERE name = 'civibooking_resource_criteria';
-DELETE FROM civicrm_option_group WHERE name = 'civibooking_resource_location';
-DELETE FROM civicrm_option_group WHERE name = 'civibooking_cancellation_charges';
-DELETE FROM civicrm_option_group WHERE name = 'civibooking_size_unit';
+DELETE FROM civicrm_option_group WHERE name = 'booking_booking_status';
+DELETE FROM civicrm_option_group WHERE name = 'booking_resource_type';
+DELETE FROM civicrm_option_group WHERE name = 'booking_resource_criteria';
+DELETE FROM civicrm_option_group WHERE name = 'booking_resource_location';
+DELETE FROM civicrm_option_group WHERE name = 'booking_cancellation_charges';
+DELETE FROM civicrm_option_group WHERE name = 'booking_size_unit';
 
 
 SET FOREIGN_KEY_CHECKS=1;
