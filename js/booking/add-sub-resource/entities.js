ae385706bbbab39990e6aeed13d026750b561bc8
Erawat Chamanont <erawat@gmail.com>
2013-08-22 22:28:25 +0100

    Add attributes to SubResource entity

--- js/booking/add-sub-resource/entities.js
+++ js/booking/add-sub-resource/entities.js
@@ -3,19 +3,23 @@ CRM.BookingApp.module('Entities', function(Entities, BookingApp, Backbone, Mario
   Entities.SubResource = Backbone.Model.extend({
     defaults: {
       sub_resources: {},
+      sub_total: 0,
+      adhoc_charges:0,
+      manual_adjustment:0,
+      total:0
     },
   });
 
   Entities.AddSubResource = Backbone.Model.extend({
     defaults: {
-      'parent_ref_id': null,
-      'ref_id': null,
-      'resource': {id : null, label :null},
-      'configuration': {id : null, label :null, price :0},
-      'quantity': 0,
-      'time_reuired': null,
-      'note': null,
-      'price_estimate': 0,
+      parent_ref_id: null,
+      ref_id: null,
+      resource: {id : null, label :null},
+      configuration: {id : null, label :null, price :0},
+      quantity: 0,
+      time_reuired: null,
+      note: null,
+      price_estimate: 0,
     },
   });
 
