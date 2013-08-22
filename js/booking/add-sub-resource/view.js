b8871081cefd0e049f3416f5717b0b7c2eaabfee
Erawat Chamanont <erawat@gmail.com>
2013-08-22 15:28:30 +0100

    Calulate the estimate price on the option selected

--- js/booking/add-sub-resource/view.js
+++ js/booking/add-sub-resource/view.js
@@ -1,12 +1,15 @@
 CRM.BookingApp.module('AddSubResource', function(AddSubResource, BookingApp, Backbone, Marionette, $, _) {
 
+   CRM.BookingApp.vent.on("bind:datepicker", function (context){
+    //context.$el.find('.date').timepicker();
+   });
+
   CRM.BookingApp.vent.on("render:options", function (options){
-    var template = _.template($('#select-option-template').html());
     var select = options.context.$el.find(options.element);
     if(select.is('[disabled]')){
       select.prop('disabled', false);
     }
-    select.html(template({options: options.list, first_option: options.first_option}));
+    select.html(options.template({options: options.list, first_option: options.first_option}));
   });
 
   AddSubResource.ResourceTableView = Backbone.Marionette.ItemView.extend({
@@ -39,24 +42,56 @@ CRM.BookingApp.module('AddSubResource', function(AddSubResource, BookingApp, Bac
 
   AddSubResource.AddSubResourceModal = Backbone.Marionette.ItemView.extend({
     template: "#add-sub-resource-template",
-    //model: CRM.BookingApp.Entities.SubResourceModel,
     initialize: function(options){
       this.resources = options.resources;
+      this.model = new CRM.BookingApp.Entities.SubResourceModel();
     },
-
     events: {
       'click #add-to-basket': 'addSubResource',
-      'click .cancel': 'addSubResource',
       'change #resourceSelect': 'getConfigurations',
+      'change #configSelect': 'updatePriceEstmate',
+      'keypress #quantity': 'updatePriceEstmate',
+      'keyup #quantity': 'updatePriceEstmate',
+      'keydown #quantity': 'updatePriceEstmate',
+
     },
     onRender: function(){
-      var params = {context:this, list: this.resources, element: "#resourceSelect", first_option: '- ' + ts('select resource') + ' -'}
+     var tpl = _.template($('#select-option-template').html());
+      var params = {
+        context:this,
+        template: tpl,
+        list:this.resources,
+        element: "#resourceSelect",
+        first_option: '- ' + ts('select resource') + ' -'
+      }
       CRM.BookingApp.vent.trigger("render:options", params);
-    },
+      CRM.BookingApp.vent.trigger("bind:datepicker", this);
 
+    },
+    updatePriceEstmate: function(e){
+      var qualitySelector = this.$el.find('#quantity');
+      if(e.type == 'change'){
+        var configSelect = this.$el.find('#configSelect');
+        if(configSelect.val() !== ''){
+          configSelect.find(':selected').data('price');
+          var price = configSelect.find(':selected').data('price');
+          this.model.set('configuration', configSelect.val());
+          this.model.set('configuration_price', price);
+          qualitySelector.prop('disabled', false);
+        }else{
+          qualitySelector.prop('disabled', true);
+          qualitySelector.val('');
+        }
+      }
+      var priceEstimate = this.model.get('quantity') * this.model.get('configuration_price');
+      this.model.set('quantity', qualitySelector.val());
+      this.model.set('price_estimate', priceEstimate);
+      this.$el.find('#price-estimate').html(priceEstimate);
+    },
     getConfigurations: function(e){
       selectedVal = $('#resourceSelect').val();
       if(selectedVal !== ""){
+        this.model.set('resource', selectedVal);
         var params = {
               id: selectedVal,
               sequential: 1,
@@ -72,18 +107,31 @@ CRM.BookingApp.module('AddSubResource', function(AddSubResource, BookingApp, Bac
           { context: self,
             success: function(data) {
             var options = data['values']['0']['api.booking_resource_config_set.get']['values']['0']['api.booking_resource_config_option.get']['values'];
-            var params = {context:this, list: options, element: "#configSelect", first_option: '- ' + ts('select configuration') + ' -'}
+
+            var params = {
+              context:this,
+              template: _.template($('#select-config-option-template').html()),
+              list: options,
+              element: "#configSelect",
+              first_option: '- ' + ts('select configuration') + ' -'
+            }
             CRM.BookingApp.vent.trigger("render:options", params);
 
           }
         });
       }else{
-        var params = {context:this, list: new Array(), element: "#configSelect", first_option: '- ' + ts('select configuration') + ' -'}
+        var params = {
+          context:this,
+          template: _.template($('#select-config-option-template').html()),
+          list: new Array(),
+          element: "#configSelect",
+          first_option: '- ' + ts('select configuration') + ' -'}
         CRM.BookingApp.vent.trigger("render:options", params);
         this.$el.find('#configSelect').prop('disabled', true);
       }
     },
     addSubResource: function(e){
+      console.log(this.model);
      //CRM.BookingApp.modal.close(this);
     }
 
