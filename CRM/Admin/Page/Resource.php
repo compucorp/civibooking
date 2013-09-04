b8f16a0746f44add4cb2adb6855b5abcc795b76c
Erawat Chamanont <erawat@gmail.com>
2013-09-04 17:55:41 +0100

    Display list of resources

---
+++ CRM/Admin/Page/Resource.php
@@ -0,0 +1,185 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.4                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+
+/**
+ * Page for displaying list of resources
+ */
+class CRM_Admin_Page_Resource extends CRM_Core_Page_Basic {
+
+  /**
+   * The action links that we need to display for the browse screen
+   *
+   * @var array
+   * @static
+   */
+  static $_links = NULL;
+
+  /**
+   * Get BAO Name
+   *
+   * @return string Classname of BAO.
+   */
+  function getBAOName() {
+    return 'CRM_Booking_BAO_Resource';
+  }
+
+  /**
+   * Get action Links
+   *
+   * @return array (reference) of action links
+   */
+  function &links() {
+    if (!(self::$_links)) {
+      self::$_links = array(
+        CRM_Core_Action::UPDATE => array(
+          'name' => ts('Edit'),
+          'url' => 'civicrm/admin/resource',
+          'qs' => 'action=update&id=%%id%%&reset=1',
+          'title' => ts('Edit Resource'),
+        ),
+        CRM_Core_Action::DISABLE => array(
+          'name' => ts('Disable'),
+          'extra' => 'onclick = "enableDisable( %%id%%,\'' . 'CRM_Booking_BAO_Resource' . '\',\'' . 'enable-disable' . '\' );"',
+          'ref' => 'disable-action',
+          'title' => ts('Disable Resource'),
+        ),
+        CRM_Core_Action::ENABLE => array(
+          'name' => ts('Enable'),
+          'extra' => 'onclick = "enableDisable( %%id%%,\'' . 'CRM_Booking_BAO_Resource' . '\',\'' . 'disable-enable' . '\' );"',
+          'ref' => 'enable-action',
+          'title' => ts('Enable Resource'),
+        ),
+        CRM_Core_Action::DELETE => array(
+          'name' => ts('Delete'),
+          'url' => 'civicrm/admin/resource',
+          'qs' => 'action=delete&id=%%id%%',
+          'title' => ts('Delete Resource'),
+        ),
+      );
+    }
+    return self::$_links;
+  }
+
+  /**
+   * Run the page.
+   *
+   * This method is called after the page is created. It checks for the
+   * type of action and executes that action.
+   * Finally it calls the parent's run method.
+   *
+   * @return void
+   * @access public
+   *
+   */
+  function run() {
+    // set title and breadcrumb
+    CRM_Utils_System::setTitle(ts('Settings - Manage Resource'));
+    $breadCrumb = array(array('title' => ts('Administration'),
+        'url' => CRM_Utils_System::url('civicrm/admin',
+        'reset=1'
+        ),
+      ));
+    CRM_Utils_System::appendBreadCrumb($breadCrumb);
+    return parent::run();
+  }
+
+  /**
+   * Browse all resources.
+   *
+   * @return void
+   * @access public
+   * @static
+   */
+  function browse($action = NULL) {
+    // get all custom groups sorted by weight
+    $resources = array();
+    $dao = new CRM_Booking_DAO_Resource();
+    $dao->orderBy('weight');
+    $dao->find();
+
+    while ($dao->fetch()) {
+
+      $resources[$dao->id] = array();
+      CRM_Core_DAO::storeValues($dao, $resources[$dao->id]);
+      //TODO:: GET Actual type and location
+
+      // form all action links
+      $action = array_sum(array_keys($this->links()));
+
+      // update enable/disable links.
+      if ($dao->is_active) {
+        $action -= CRM_Core_Action::ENABLE;
+      }
+      else {
+        $action -= CRM_Core_Action::DISABLE;
+      }
+
+      $resources[$dao->id]['action'] = CRM_Core_Action::formLink(self::links(), $action,
+        array('id' => $dao->id)
+      );
+
+    }
+
+    $this->assign('rows', $resources);
+  }
+
+  /**
+   * Get name of edit form
+   *
+   * @return string Classname of edit form.
+   */
+  function editForm() {
+    return 'CRM_Admin_Form_Resource';
+  }
+
+  /**
+   * Get edit form name
+   *
+   * @return string name of this page.
+   */
+  function editName() {
+    return 'Resource';
+  }
+
+  /**
+   * Get user context.
+   *
+   * @return string user context.
+   */
+  function userContext($mode = NULL) {
+    return 'civicrm/admin/resource';
+  }
+}
+
