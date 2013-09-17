a7c4eb5cbce5c93a9d23041f7e7e60bbf4e928a3
Erawat Chamanont <erawat@gmail.com>
2013-09-17 19:32:43 +0100

    updated the new url for loading the tab.

--- booking.php
+++ booking.php
@@ -18,7 +18,7 @@ function booking_civicrm_tabs(&$tabs, $cid) {
       'weight' => '998',
     );
     //if ($count > 0) {
-    $tab['url'] = CRM_Utils_System::url('civicrm/booking/tab', "reset=1&cid={$cid}&snippet=1&force=1", false, null, false);
+    $tab['url'] = CRM_Utils_System::url('civicrm/contact/view/booking', "reset=1&cid={$cid}&snippet=1&force=1", false, null, false);
     //}
     $tabs[] = $tab;
 }
@@ -28,7 +28,7 @@ function booking_civicrm_tabs(&$tabs, $cid) {
  *
  */
 function cividiscount_civicrm_perm() {
-  return array('view CiviBooking', 'administer CiviBooking');
+  return array('view booking', 'administer CiviBooking');
 }
 
 
