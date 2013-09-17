ef63612d53aba4d64b985dd31c3d54473697c115
Erawat Chamanont <erawat@gmail.com>
2013-09-17 19:31:48 +0100

    add  getSubSlotSlot function

--- CRM/Booking/BAO/SubSlot.php
+++ CRM/Booking/BAO/SubSlot.php
@@ -24,4 +24,36 @@ class CRM_Booking_BAO_SubSlot extends CRM_Booking_DAO_SubSlot {
   }
 
 
+   static function getSubSlotSlot($slotId){
+    $params = array(1 => array( $slotId, 'Integer'));
+
+    $query = "
+      SELECT civicrm_booking_sub_slot.id,
+             civicrm_booking_sub_slot.slot_id,
+             civicrm_booking_sub_slot.resource_id,
+             civicrm_booking_sub_slot.config_id,
+             civicrm_booking_sub_slot.time_required,
+             civicrm_booking_sub_slot.note
+      FROM civicrm_booking_sub_slot
+      INNER JOIN civicrm_booking_slot ON civicrm_booking_slot.id = civicrm_booking_sub_slot.slot_id
+      WHERE 1
+      AND civicrm_booking_slot.id = %1
+     ";
+
+    $slots = array();
+    $dao = CRM_Core_DAO::executeQuery($query, $params);
+    while ($dao->fetch()) {
+      $slots[$dao->id] = array(
+        'id' => $dao->id,
+        'slot_id' => $dao->slot_id,
+        'resource_id' => $dao->resource_id,
+        'config_id' => $dao->config_id,
+        'time_required' => $dao->time_required,
+        'note' => $dao->note,
+      );
+    }
+    return $slots;
+  }
+
+
 }
