cbdb3330e71972c79be108776f2f8d4ab965d480
Erawat Chamanont <erawat@gmail.com>
2013-09-19 12:30:25 +0100

    add remove msg_template when uninstall the extension

--- sql/civibooking_uninstall.sql
+++ sql/civibooking_uninstall.sql
@@ -40,16 +40,17 @@ DELETE civicrm_option_value FROM civicrm_option_value
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
 WHERE civicrm_option_group.name = 'booking_size_unit';
 
-DELETE civicrm_option_value FROM civicrm_option_value
-INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
-WHERE civicrm_option_group.name = 'msg_tpl_workflow_booking';
-
 DELETE civicrm_msg_template FROM civicrm_msg_template
 INNER JOIN civicrm_option_value ON civicrm_option_value.id = civicrm_msg_template.workflow_id
 INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
 WHERE civicrm_option_group.name = 'msg_tpl_workflow_booking';
 
+DELETE civicrm_option_value FROM civicrm_option_value
+INNER JOIN civicrm_option_group ON civicrm_option_group.id = civicrm_option_value.option_group_id
+WHERE civicrm_option_group.name = 'msg_tpl_workflow_booking';
+
 --all option groups related to civibooking
+
 DELETE FROM civicrm_option_group WHERE name = 'booking_status';
 DELETE FROM civicrm_option_group WHERE name = 'booking_resource_type';
 DELETE FROM civicrm_option_group WHERE name = 'booking_resource_criteria';
