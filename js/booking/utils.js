d2c2799d455e0752b29cf3d04e23fad8edae3adc
Erawat Chamanont <erawat@gmail.com>
2013-09-19 16:04:58 +0100

    created a until for checking if input value is valid integer

--- js/booking/utils.js
+++ js/booking/utils.js
@@ -1,8 +1,13 @@
-//http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
 CRM.BookingApp.module("Utils", function(Utils, BookingApp, Backbone, Marionette, $, _){
 
+  //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
   Utils.getCurrentUnixTimstamp = function () {
     return unix = Math.round(+new Date()/1000);
+  }
+
+  //http://stackoverflow.com/questions/10834796/validate-that-a-string-is-a-positive-integer
+  Utils.isPositiveInteger = function (n){
+   return 0 === n % (!isNaN(parseFloat(n)) && 0 <= ~~n);
   }
 
 });
