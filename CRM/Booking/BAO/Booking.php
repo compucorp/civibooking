49b082781181ea8d34d3c0180904662926bacc7f
Erawat Chamanont <erawat@gmail.com>
2013-09-20 14:40:17 +0100

    create adhoc charges record when creating booking

--- CRM/Booking/BAO/Booking.php
+++ CRM/Booking/BAO/Booking.php
@@ -33,12 +33,28 @@ class CRM_Booking_BAO_Booking extends CRM_Booking_DAO_Booking {
    */
   static function create(&$params) {
 
-    $resources = $params['resources'];
 
+    $resources = $params['resources'];
+    $adhocCharges = $params['adhoc_charges'];
+
+    if($params['validate']){
+      //TODO:: Validate resource
+      //$result = array();
+      //$isValid = CRM_Booking_BAO_Slot::validate($resources, $result);
+      //if(!$result['isValid']){
+        //return list of object that invalid
+        ///return error message
+      //}
+    }
     unset($params['resources']);
     unset($params['version']);
-
+    unset($params['adhoc_charges']);
+    unset($params['validate']);
     $transaction = new CRM_Core_Transaction();
+    $lineItem = array(
+        'version' => 3,
+        'sequential' => 1,
+    );
     try{
       $booking = self::add($params);
       $bookingID = $booking->id;
@@ -51,28 +67,88 @@ class CRM_Booking_BAO_Booking extends CRM_Booking_DAO_Booking {
           'start' => CRM_Utils_Array::value('start_date', $resource),
           'end' => CRM_Utils_Array::value('end_date', $resource),
           'resource_id' =>  CRM_Utils_Array::value('resource_id', $resource),
-          //'quantity' => CRM_Utils_Array::value('quantity', $resource), should go to price set
-          //'note' => CRM_Utils_Array::value('note', $resource), //TODO::Passing note from the UK
+          'quantity' => CRM_Utils_Array::value('quantity', $resource),
+          'note' => CRM_Utils_Array::value('note', $resource),
         );
         $slotResult = civicrm_api('Slot', 'Create', $slot);
-          $slotID =  CRM_Utils_Array::value('id', $slotResult);
-          $subResources = $resource['sub_resources'];
-          foreach($subResources as $subKey => $subResource){
-            $subSlot = array(
-              'version' => 3,
-              'resource_id' =>  CRM_Utils_Array::value('resource_id', $resource),
-              'slot_id' => $slotID,
-              'config_id' => CRM_Utils_Array::value('configuration_id', $resource),
-              'time_required' => date() //FIXED:: get the time from the UI
-              //'note' => CRM_Utils_Array::value('note', $resource), //TODO::Passing note from the UI
-            );
-            $subSlotResult = civicrm_api('SubSlot', 'Create', $subSlot);
-          }
+        $slotID =  CRM_Utils_Array::value('id', $slotResult);
+
+        $subResources = $resource['sub_resources'];
+        foreach($subResources as $subKey => $subResource){
+          $subSlot = array(
+            'version' => 3,
+            'resource_id' =>  CRM_Utils_Array::value('resource_id', $subResource),
+            'slot_id' => $slotID,
+            'config_id' => CRM_Utils_Array::value('configuration_id', $subResource),
+            'time_required' =>  CRM_Utils_Array::value('time_required', $subResource),
+            'quantity' => CRM_Utils_Array::value('quantity', $subResource),
+            'note' => CRM_Utils_Array::value('note', $subResource),
+          );
+          $subSlotResult = civicrm_api('SubSlot', 'Create', $subSlot);
+        }
+      }
+      if($adhocCharges){
+        $items = CRM_Utils_Array::value('items', $adhocCharges);
+        foreach ($items as $key => $item) {
+          $params = array(
+            'version' => 3,
+            'booking_id' =>  $bookingID,
+            'item_id' => CRM_Utils_Array::value('id', $item),
+            'quantity' => CRM_Utils_Array::value('quantity', $item),
+          );
+          civicrm_api('AdhocCharges', 'create', $params);
+        }
       }
       return $booking;
     }catch (Exception $e) {
       $transaction->rollback();
       CRM_Core_Error::fatal($e->getMessage());
+    }
+
+  }
+
+  static function recordContribution($values){
+    $bookingID = CRM_Utils_Array::value('booking_id', $values);
+    if(!CRM_Utils_Array::value('booking_id', $values)){
+      return;
+    }else{
+    $slots = CRM_Booking_BAO_Slot::getBookingSlot()
+
+    /*
+        if($slotID){
+          $lineItem['entity_table'] = "civicrm_booking_slot";
+          $lineItem['entity_id'] = $slotID;
+          $lineItem['qty'] =  CRM_Utils_Array::value('quantity', $resource);
+          //TODO get unit price;
+          $lineItem['unit_price'] =  CRM_Utils_Array::value('quantity', $resource);
+          $lineItem['line_total'] =  CRM_Utils_Array::value('price', $resource);
+          //$result = civicrm_api('LineItem', 'create', $params);
+          dpr($result);
+
+        }*/
+
+       //$subSlotId =  CRM_Utils_Array::value('id', $slotResult);
+            /*
+            if($subSlotId){
+              $lineItem['entity_table'] = "civicrm_booking_sub_slot";
+              $lineItem['entity_id'] = $subSlotId;
+              $lineItem['qty'] = CRM_Utils_Array::value('quantity', $subResource);
+              //TODO:// calulate pice;
+              //$lineItem['unit_price'] =
+              //$lineItem['line_total'] =
+              //$result = civicrm_api('LineItem', 'create', $params);
+              dpr($result);
+            *
+            }*/
+/*
+      $params = array(
+  'version' => 3,
+  'sequential' => 1,
+);
+    $result = civicrm_api('Contribution', 'create', $params);*/
+
+    dpr($values);
+    exit;
     }
 
   }
@@ -139,6 +215,306 @@ class CRM_Booking_BAO_Booking extends CRM_Booking_DAO_Booking {
     }
     return self::$_exportableFields["booking"];
   }
+
+
+  /**
+   * Function to send the emails
+   *
+   * @param array   $contactIDs        contact ids
+   * @param array   $values            associated array of fields
+   * @param boolean $isTest            if in test mode
+   * @param boolean $returnMessageText return the message text instead of sending the mail
+   *
+   * @return void
+   * @access public
+   * @static
+   */
+  static function sendMail($contactIDs, &$values, $isTest = FALSE, $returnMessageText = FALSE, $fieldTypes = NULL) {
+    $gIds = $params = array();
+    $email = NULL;
+    if (isset($values['custom_pre_id'])) {
+      $preProfileType = CRM_Core_BAO_UFField::getProfileType($values['custom_pre_id']);
+      if ($preProfileType == 'Membership' && CRM_Utils_Array::value('membership_id', $values)) {
+        $params['custom_pre_id'] = array(
+          array(
+            'member_id',
+            '=',
+            $values['membership_id'],
+            0,
+            0,
+          ),
+        );
+      }
+      elseif ($preProfileType == 'Contribution' && CRM_Utils_Array::value('contribution_id', $values)) {
+        $params['custom_pre_id'] = array(
+          array(
+            'contribution_id',
+            '=',
+            $values['contribution_id'],
+            0,
+            0,
+          ),
+        );
+      }
+
+      $gIds['custom_pre_id'] = $values['custom_pre_id'];
+    }
+
+    if (isset($values['custom_post_id'])) {
+      $postProfileType = CRM_Core_BAO_UFField::getProfileType($values['custom_post_id']);
+      if ($postProfileType == 'Membership' && CRM_Utils_Array::value('membership_id', $values)) {
+        $params['custom_post_id'] = array(
+          array(
+            'member_id',
+            '=',
+            $values['membership_id'],
+            0,
+            0,
+          ),
+        );
+      }
+      elseif ($postProfileType == 'Contribution' && CRM_Utils_Array::value('contribution_id', $values)) {
+        $params['custom_post_id'] = array(
+          array(
+            'contribution_id',
+            '=',
+            $values['contribution_id'],
+            0,
+            0,
+          ),
+        );
+      }
+
+      $gIds['custom_post_id'] = $values['custom_post_id'];
+    }
+
+    if (CRM_Utils_Array::value('is_for_organization', $values)) {
+      if (CRM_Utils_Array::value('membership_id', $values)) {
+        $params['onbehalf_profile'] = array(
+          array(
+            'member_id',
+            '=',
+            $values['membership_id'],
+            0,
+            0,
+          ),
+        );
+      }
+      elseif (CRM_Utils_Array::value('contribution_id', $values)) {
+        $params['onbehalf_profile'] = array(
+          array(
+            'contribution_id',
+            '=',
+            $values['contribution_id'],
+            0,
+            0,
+          ),
+        );
+      }
+    }
+
+    //check whether it is a test drive
+    if ($isTest && !empty($params['custom_pre_id'])) {
+      $params['custom_pre_id'][] = array(
+        'contribution_test',
+        '=',
+        1,
+        0,
+        0,
+      );
+    }
+
+    if ($isTest && !empty($params['custom_post_id'])) {
+      $params['custom_post_id'][] = array(
+        'contribution_test',
+        '=',
+        1,
+        0,
+        0,
+      );
+    }
+
+    if (!$returnMessageText && !empty($gIds)) {
+      //send notification email if field values are set (CRM-1941)
+      foreach ($gIds as $key => $gId) {
+        if ($gId) {
+          $email = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_UFGroup', $gId, 'notify');
+          if ($email) {
+            $val = CRM_Core_BAO_UFGroup::checkFieldsEmptyValues($gId, $contactID, CRM_Utils_Array::value($key, $params), true );
+            CRM_Core_BAO_UFGroup::commonSendMail($contactID, $val);
+          }
+        }
+      }
+    }
+
+    if ( CRM_Utils_Array::value('is_email_receipt', $values) ||
+      CRM_Utils_Array::value('onbehalf_dupe_alert', $values) ||
+      $returnMessageText
+    ) {
+      $template = CRM_Core_Smarty::singleton();
+
+      // get the billing location type
+      if (!array_key_exists('related_contact', $values)) {
+        $locationTypes = CRM_Core_PseudoConstant::get('CRM_Core_DAO_Address', 'location_type_id');
+        $billingLocationTypeId = array_search('Billing', $locationTypes);
+      }
+      else {
+        // presence of related contact implies onbehalf of org case,
+        // where location type is set to default.
+        $locType = CRM_Core_BAO_LocationType::getDefault();
+        $billingLocationTypeId = $locType->id;
+      }
+
+      if (!array_key_exists('related_contact', $values)) {
+        list($displayName, $email) = CRM_Contact_BAO_Contact_Location::getEmailDetails($contactID, FALSE, $billingLocationTypeId);
+      }
+      // get primary location email if no email exist( for billing location).
+      if (!$email) {
+        list($displayName, $email) = CRM_Contact_BAO_Contact_Location::getEmailDetails($contactID);
+      }
+      if (empty($displayName)) {
+        list($displayName, $email) = CRM_Contact_BAO_Contact_Location::getEmailDetails($contactID);
+      }
+
+      //for display profile need to get individual contact id,
+      //hence get it from related_contact if on behalf of org true CRM-3767
+      //CRM-5001 Contribution/Membership:: On Behalf of Organization,
+      //If profile GROUP contain the Individual type then consider the
+      //profile is of Individual ( including the custom data of membership/contribution )
+      //IF Individual type not present in profile then it is consider as Organization data.
+      $userID = $contactID;
+      if ($preID = CRM_Utils_Array::value('custom_pre_id', $values)) {
+        if (CRM_Utils_Array::value('related_contact', $values)) {
+          $preProfileTypes = CRM_Core_BAO_UFGroup::profileGroups($preID);
+          if (in_array('Individual', $preProfileTypes) || in_array('Contact', $postProfileTypes)) {
+            //Take Individual contact ID
+            $userID = CRM_Utils_Array::value('related_contact', $values);
+          }
+        }
+        self::buildCustomDisplay($preID, 'customPre', $userID, $template, $params['custom_pre_id']);
+      }
+      $userID = $contactID;
+      if ($postID = CRM_Utils_Array::value('custom_post_id', $values)) {
+        if (CRM_Utils_Array::value('related_contact', $values)) {
+          $postProfileTypes = CRM_Core_BAO_UFGroup::profileGroups($postID);
+          if (in_array('Individual', $postProfileTypes) || in_array('Contact', $postProfileTypes)) {
+            //Take Individual contact ID
+            $userID = CRM_Utils_Array::value('related_contact', $values);
+          }
+        }
+        self::buildCustomDisplay($postID, 'customPost', $userID, $template, $params['custom_post_id']);
+      }
+
+      $title = isset($values['title']) ? $values['title'] : CRM_Contribute_PseudoConstant::contributionPage($values['contribution_page_id']);
+
+      // set email in the template here
+      $tplParams = array(
+        'email' => $email,
+        'receiptFromEmail' => CRM_Utils_Array::value('receipt_from_email', $values),
+        'contactID' => $contactID,
+        'displayName' => $displayName,
+        'contributionID' => CRM_Utils_Array::value('contribution_id', $values),
+        'contributionOtherID' => CRM_Utils_Array::value('contribution_other_id', $values),
+        'membershipID' => CRM_Utils_Array::value('membership_id', $values),
+        // CRM-5095
+        'lineItem' => CRM_Utils_Array::value('lineItem', $values),
+        // CRM-5095
+        'priceSetID' => CRM_Utils_Array::value('priceSetID', $values),
+        'title' => $title,
+        'isShare' => CRM_Utils_Array::value('is_share', $values),
+      );
+
+      if ($contributionTypeId = CRM_Utils_Array::value('financial_type_id', $values)) {
+        $tplParams['contributionTypeId'] = $contributionTypeId;
+        $tplParams['contributionTypeName'] = CRM_Core_DAO::getFieldValue('CRM_Financial_DAO_FinancialType',
+          $contributionTypeId);
+      }
+
+      if ($contributionPageId = CRM_Utils_Array::value('id', $values)) {
+        $tplParams['contributionPageId'] = $contributionPageId;
+      }
+
+      // address required during receipt processing (pdf and email receipt)
+      if ($displayAddress = CRM_Utils_Array::value('address', $values)) {
+        $tplParams['address'] = $displayAddress;
+      }
+
+      // CRM-6976
+      $originalCCReceipt = CRM_Utils_Array::value('cc_receipt', $values);
+
+      // cc to related contacts of contributor OR the one who
+      // signs up. Is used for cases like - on behalf of
+      // contribution / signup ..etc
+      if (array_key_exists('related_contact', $values)) {
+        list($ccDisplayName, $ccEmail) = CRM_Contact_BAO_Contact_Location::getEmailDetails($values['related_contact']);
+        $ccMailId = "{$ccDisplayName} <{$ccEmail}>";
+
+        $values['cc_receipt'] = CRM_Utils_Array::value('cc_receipt', $values) ? ($values['cc_receipt'] . ',' . $ccMailId) : $ccMailId;
+
+        // reset primary-email in the template
+        $tplParams['email'] = $ccEmail;
+
+        $tplParams['onBehalfName'] = $displayName;
+        $tplParams['onBehalfEmail'] = $email;
+
+        $ufJoinParams = array(
+          'module' => 'onBehalf',
+          'entity_table' => 'civicrm_contribution_page',
+          'entity_id' => $values['id'],
+        );
+        $OnBehalfProfile = CRM_Core_BAO_UFJoin::getUFGroupIds($ufJoinParams);
+        $profileId = $OnBehalfProfile[0];
+        $userID = $contactID;
+        self::buildCustomDisplay($profileId, 'onBehalfProfile', $userID, $template, $params['onbehalf_profile'], $fieldTypes);
+      }
+
+      // use either the contribution or membership receipt, based on whether it’s a membership-related contrib or not
+      $sendTemplateParams = array(
+        'groupName' => $tplParams['membershipID'] ? 'msg_tpl_workflow_membership' : 'msg_tpl_workflow_contribution',
+        'valueName' => $tplParams['membershipID'] ? 'membership_online_receipt' : 'contribution_online_receipt',
+        'contactId' => $contactID,
+        'tplParams' => but ,
+        'isTest' => $isTest,
+        'PDFFilename' => 'receipt.pdf',
+      );
+
+      if ($returnMessageText) {
+        list($sent, $subject, $message, $html) = CRM_Core_BAO_MessageTemplate::sendTemplate($sendTemplateParams);
+        return array(
+          'subject' => $subject,
+          'body' => $message,
+          'to' => $displayName,
+          'html' => $html,
+        );
+      }
+
+      if ($values['is_email_receipt']) {
+        $sendTemplateParams['from'] = CRM_Utils_Array::value('receipt_from_name', $values) . ' <' . $values['receipt_from_email'] . '>';
+        $sendTemplateParams['toName'] = $displayName;
+        $sendTemplateParams['toEmail'] = $email;
+        $sendTemplateParams['cc'] = CRM_Utils_Array::value('cc_receipt', $values);
+        $sendTemplateParams['bcc'] = CRM_Utils_Array::value('bcc_receipt', $values);
+        list($sent, $subject, $message, $html) = CRM_Core_BAO_MessageTemplate::sendTemplate($sendTemplateParams);
+      }
+
+      // send duplicate alert, if dupe match found during on-behalf-of processing.
+      if (CRM_Utils_Array::value('onbehalf_dupe_alert', $values)) {
+        $sendTemplateParams['groupName'] = 'msg_tpl_workflow_contribution';
+        $sendTemplateParams['valueName'] = 'contribution_dupalert';
+        $sendTemplateParams['from'] = ts('Automatically Generated') . " <{$values['receipt_from_email']}>";
+        $sendTemplateParams['toName'] = CRM_Utils_Array::value('receipt_from_name', $values);
+        $sendTemplateParams['toEmail'] = CRM_Utils_Array::value('receipt_from_email', $values);
+        $sendTemplateParams['tplParams']['onBehalfID'] = $contactID;
+        $sendTemplateParams['tplParams']['receiptMessage'] = $message;
+
+        // fix cc and reset back to original, CRM-6976
+        $sendTemplateParams['cc'] = $originalCCReceipt;
+
+        CRM_Core_BAO_MessageTemplate::sendTemplate($sendTemplateParams);
+      }
+    }
+  }
+
 
 
 }
