846fc7a54e6a0dccf867050a05fcc3a558d72da1
Erawat Chamanont <erawat@gmail.com>
2013-09-23 16:59:46 +0100

    update the field label

--- CRM/Booking/Form/Booking/Cancel.php
+++ CRM/Booking/Form/Booking/Cancel.php
@@ -59,14 +59,14 @@ class CRM_Booking_Form_Booking_Cancel extends CRM_Booking_Form_Booking_Base {
   public function buildQuickForm() {
     parent::buildQuickForm();
 
-    $this->add('text', 'event_date', ts('Date of event'), array('disabled' => 'disabled'));
-    $this->addDate('cancellation_date', ts('Date of cancellation'), FALSE, array('formatType' => 'activityDate'));
+    $this->add('text', 'event_date', ts('Date of Event'), array('disabled' => 'disabled'));
+    $this->addDate('cancellation_date', ts('Date of Cancellation'), FALSE, array('formatType' => 'activityDate'));
 
-    $this->add('text', 'resource_fee', ts('Resource fees'), array('disabled' => 'disabled'));
-    $this->add('text', 'sub_resource_fee', ts('Sub resoruce fees'), array('disabled' => 'disabled'));
-    $this->add('text', 'adhoc_charges', ts('Ad-hoc charges'), array('disabled' => 'disabled'));
-    $this->add('text', 'discount_amount', ts('Discount amount'), array('disabled' => 'disabled'));
-    $this->add('text', 'booking_total', ts('Booking value'), array('disabled' => 'disabled'));
+    $this->add('text', 'resource_fee', ts('Resource Fees'), array('disabled' => 'disabled'));
+    $this->add('text', 'sub_resource_fee', ts('Sub Resource Fees'), array('disabled' => 'disabled'));
+    $this->add('text', 'adhoc_charges', ts('Ad-hoc Charges'), array('disabled' => 'disabled'));
+    $this->add('text', 'discount_amount', ts('Discount Amount'), array('disabled' => 'disabled'));
+    $this->add('text', 'booking_total', ts('Booking Amount'), array('disabled' => 'disabled'));
 
     $result = civicrm_api('OptionValue', 'get',  array(
       'version' => 3,
@@ -82,11 +82,11 @@ class CRM_Booking_Form_Booking_Cancel extends CRM_Booking_Form_Booking_Base {
       array()
     );
 
-    $this->add('text', 'cancellation_charge', ts('Cancellation charge'), array('disabled' => 'disabled'));
+    $this->add('text', 'cancellation_charge', ts('Cancellation Charge'), array('disabled' => 'disabled'));
     $this->add('text', 'adjustment', ts('Additional Charges/Adjectments'));
-    $this->add('textarea', 'comment', ts('Charge comment'));
+    $this->add('textarea', 'comment', ts('Charge Comment'));
 
-    $this->add('text', 'charge_amount', ts('Amount to pay'), array('disabled' => 'disabled'));
+    $this->add('text', 'charge_amount', ts('Amount to Pay'), array('disabled' => 'disabled'));
 
   }
 
