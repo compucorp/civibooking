594f26d8fdec79c613968e5710857ac697ddcb9b
Erawat Chamanont <erawat@gmail.com>
2013-09-24 17:54:16 +0100

    called parent postProcess for process record contribution and sending email

--- CRM/Booking/Form/Booking/Update.php
+++ CRM/Booking/Form/Booking/Update.php
@@ -94,10 +94,10 @@ class CRM_Booking_Form_Booking_Update extends CRM_Booking_Form_Booking_Base {
       $params['id'] = $this->_id;
       $params['status_id'] = $values['booking_status'];
       $booking = CRM_Booking_BAO_Booking::add($params);
+      parent::postProcess();
       CRM_Core_Session::setStatus(ts('The booking \'%1\' has been saved.', array(1 => $booking->id)), ts('Saved'), 'success');
 
     }
-
   }
 
 }
