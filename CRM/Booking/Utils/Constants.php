fb5f3c373d4cdd1649347d29e119f435aa9ef93c
Erawat Chamanont <erawat@gmail.com>
2013-10-07 13:15:14 +0100

    Added the constants class for handing the option group/activity name that use accross the booking

---
+++ CRM/Booking/Utils/Constants.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.4                                              |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CDM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+
+class CRM_Booking_Utils_Constants{
+
+  const CUSTOM_ACTIVITY_TYPE = 'booking_acivity_booking';
+  const OPTION_BOOKING_STATUS = 'booking_status';
+  const OPTION_RESOURCE_TYPE = 'booking_resource_type';
+  const OPTION_RESOURCE_LOCATION = 'booking_resource_location';
+  const OPTION_RESOURCE_CRITERIA = 'booking_resource_criteria';
+  const OPTION_SIZE_UNIT = 'booking_size_unit';
+  const OPTION_CANCELLATION_CHARGES = 'booking_cancellation_charges';
+  const OPTION_MSG_TPL_WORKFLOW_BOOKING = 'msg_tpl_workflow_booking';
+}
