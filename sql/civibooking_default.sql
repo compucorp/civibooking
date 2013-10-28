4c96fddf7dbd38990e51bda84cbb3596699fca88
Erawat Chamanont <erawat@gmail.com>
2013-10-28 18:26:28 +0000

    added the default config so the booking should be working properly

--- sql/civibooking_default.sql
+++ sql/civibooking_default.sql
@@ -1,2 +1,2 @@
-INSERT INTO `civicrm_booking_config` (`id`, `domain_id`, `day_start_at`, `day_end_at`, `log_confirmation_email`, `cc_email_address`, `bcc_email_address`, `slot_new_colour`, `slot_being_edited_colour`, `slot_booked_colour`, `slot_provisional_colour`, `slot_unavailable_colour`) VALUES
-(1, NULL, '08:00:00', '22:00:00', 0, NULL, NULL, NULL, '#0000ff', '#ff0000', '#448026', NULL);
+INSERT INTO `civicrm_booking_config` (`id`, `domain_id`, `day_start_at`, `day_end_at`, `time_period`, `log_confirmation_email`, `cc_email_address`, `bcc_email_address`, `slot_new_colour`, `slot_being_edited_colour`, `slot_booked_colour`, `slot_provisional_colour`, `slot_unavailable_colour`) VALUES
+(1, NULL, '08:00:00', '22:00:00', 30, 0, NULL, NULL, '#ff7000', '#0000ff', '#ff0000', '#448026', NULL);
