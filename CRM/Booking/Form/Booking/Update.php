b3068053bf9afefe269682f5c4c68bb2987b00c9
Erawat Chamanont <erawat@gmail.com>
2013-11-04 12:14:57 +0000

    using api3 and show booking title in the indicator

--- CRM/Booking/Form/Booking/Update.php
+++ CRM/Booking/Form/Booking/Update.php
@@ -89,16 +89,16 @@ class CRM_Booking_Form_Booking_Update extends CRM_Booking_Form_Booking_Base {
   function postProcess(){
     CRM_Utils_System::flushCache();
     if ($this->_action & CRM_Core_Action::DELETE) {
-      civicrm_api('Booking', 'delete', array('id' => $this->_id));
+      civicrm_api3('Booking', 'delete', array('id' => $this->_id));
       CRM_Core_Session::setStatus(ts('Selected booking has been deleted.'), ts('Record Deleted'), 'success');
     }
     else {
       $values = $this->exportValues();
       $params['id'] = $this->_id;
       $params['status_id'] = $values['booking_status'];
-      civicrm_api('Booking', 'create', $params);
+      $booking = civicrm_api3('Booking', 'create', $params);
       parent::postProcess();
-      CRM_Core_Session::setStatus(ts('The booking \'%1\' has been saved.', array(1 => $booking->id)), ts('Saved'), 'success');
+      CRM_Core_Session::setStatus(ts('The booking \'%1\' has been saved.', array(1 => $this->_values['title'])), ts('Saved'), 'success');
     }
   }
 
